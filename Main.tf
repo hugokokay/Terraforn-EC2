f15007add282825ce44a6ab643b5ce6a1d0c9362
Hugo Kokay <hugokoka@gmail.com>
2025-09-24 10:43:43 -0300

    111

--- Main.tf
+++ Main.tf
@@ -1,3 +1,9 @@
+provider "AWS" {
+  Region = "us-east-1"
+
+}
+
+
 resource "aws_instance" "EC2Instance" {
   ami           = "ami-08982f1c5bf93d976"
   instance_type = "t3.micro"
@@ -53,5 +59,5 @@ resource "aws_vpc_security_group_egress_rule" "InternetCLI" {
 
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = -1
-  
+
 }

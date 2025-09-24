8295c951db8df3f0a40111ca6729f0d90d30c24c
Hugo Kokay <hugokoka@gmail.com>
2025-09-24 09:37:20 -0300

    Commit 2

--- Main.tf
+++ Main.tf
@@ -7,3 +7,49 @@ resource "aws_instance" "EC2Instance" {
     Provisionado = "Terraform" 
   }
 }
+
+## Security Group 
+
+resource "aws_security_group" "SG-CLI" {
+  name        = "SG-EC2-CLI"
+  description = "Cricao de security aws_security_group"
+  vpc_id      = "vpc-003e23d66dea1e353"
+
+  tags = {
+    Name = "VPS-CLI"
+    Provisionado = "Terraform"
+  }
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "alow-SSh-CLI" {
+  security_group_id = aws_security_group.SG-CLI.id
+  cidr_ipv4         = 179.54.217.183/32
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alow-http-CLI" {
+  security_group_id = aws_security_group.SG-CLI.id
+  cidr_ipv4         = "0.0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alow-https-CLI" {
+  security_group_id = aws_security_group.SG-CLI.id
+  cidr_ipv4         = "0.0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "InternetCLI" {
+  security_group_id = aws_security_group.SG-CLI.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = -1
+}

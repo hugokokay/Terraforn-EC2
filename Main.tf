466ee43ea3014e8c1d7ccb66abe69e49ef811c38
Hugo Kokay <hugokoka@gmail.com>
2025-09-24 09:37:40 -0300

    Commit 3

--- Main.tf
+++ Main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "EC2Instance" {
   ami           = "ami-08982f1c5bf93d976"
   instance_type = "t3.micro"
+  pc_security_group_ids=[aws_security_group.SG-CLI.id]
 
   tags = {
     Name = "EC2CLI"
